d47b4f7158aa429102a63928090b5f44554bd1f9
ltagliaferri <ltagliaferri@gc.cuny.edu>
2015-11-29 01:29:01 -0500

    First commit

---
+++ HTMLToCleanTxt.pl
@@ -0,0 +1,29 @@
+use HTML::Restrict;
+#available from http://search.cpan.org/~oalders/HTML-Restrict-2.2.2/lib/HTML/Restrict.pm
+
+#Open and stringify a specific HTML file
+  local $/ = undef;
+  open $file, "AliceInWonderland.html" || die "Couldn't open file: $!";
+  binmode $file;
+  $string = <$file>;
+  close $file;
+
+
+#Process the stringified file to remove HTML tags
+my $hr = HTML::Restrict->new();
+my $processed = $hr->process($string);
+
+#Set up new txt file
+my $newfile = "AliceInWonderland.txt";
+
+#Use the open() function to create the file.
+unless(open FILE, '>'.$newfile) {
+     Die with error message 
+    # if we can't open it.
+    die "\nUnable to create $file\n";
+}
+
+# Write the cleaned raw text from the HTML file to the new file
+print FILE $processed;
+#Close the file
+close FILE;
---
+++ readme.md
@@ -0,0 +1,12 @@
+TextCleanup is developed to clean HTML from a .html file and output raw text without HTML tags into a .txt file. 
+
+Sample file of AliceInWonderland.html included.
+(Entire text of Alice’s Adventures in Wonderland via http://www.gutenberg.org/files/11/11-h/11-h.htm) 
+
+Put this script in the same directory of the file you would like to scrub and run the command 
+
+perl HTMLToCleanTxt.pl
+
+in Terminal to create raw text files to work with.
+
+Author: Lisa Tagliaferri (http://lisatagliaferri.com).
